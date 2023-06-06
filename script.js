9cce7ec4566011b1eefe203e082890b2277a1bd4
Zuzana <lakiri@gmail.com>
2023-06-06 11:53:42 +0200

    masterfiles

---
+++ script.js
@@ -0,0 +1,15 @@
+
+console.log(document.title);
+
+document.title = 'Modifying the DOM'
+console.log(document.title);
+
+document.body.style.background = 'hotpink'
+console.log(document.body.style.background);
+
+const bodyElement = document.body.children;
+
+for (let i = 0; i< bodyElement.length; i++) {
+  console.log(bodyElement[i]);
+  
+}
